41d1ee6483dccd9f322bda22f6c68f4aeda15d6f
Sebastián Marinovic <SEBAMARINOVIC.LEIVA@GMAIL.COM>
2026-02-27 17:39:41 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/mapa_bloqueo/", // <- nombre exacto del repo
-});
